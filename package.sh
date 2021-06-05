43eb6383b793414bfd9088bb17c6f291a58a68b4
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-06-06 00:27:45 +0530

    Create package.sh
    Initial commit

---
+++ package.sh
@@ -0,0 +1,29 @@
+## This shell script will prompt us to enter the package to be installed on the machine.
+
+#!/bin/bash
+
+echo "____________________ Testing __________________"
+
+read -p "Enter the package, you'd like to install on the systems: " PACKAGE
+
+sleep 1
+
+echo "Verifying the package name entered by an user" $PACKAGE
+
+sudo yum install -y $PACKAGE
+
+nslookup google.com
+
+sleep 5
+
+nslookup linkedin.com
+
+sudo yum list installed | grep $PACKAGE
+
+sleep 5
+
+echo " Going to un-install bind-utils package"
+
+sudo yum remove -y $PACKAGE
+
+echo "____________ Script Ends __________"
